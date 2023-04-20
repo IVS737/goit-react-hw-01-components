4da7da428586d499da0be3c7ad22c0fa994ef84e
IVS737 <salata.vanya191@gmail.com>
2023-04-20 16:30:40 +0300

    fix: renderItem

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,13 @@
 import styles from './Statistics.module.css';
 
-export const Statistics = props => {
-  const Arr = props.stats;
+export const Statistics = ({ stats, title }) => {
   return (
     <div className={styles.Container}>
       <section className={styles.Statistics}>
-        <h2 className={styles.Statistics__Title}>{props.title}</h2>
+        <h2 className={styles.Statistics__Title}>{title}</h2>
 
         <ul className={styles.Statistics__StatList}>
-          {Arr.map(item => {
+          {stats.map(item => {
             return (
               <li key={item.id} className={styles.Statistics__StatListItem}>
                 <span className={styles.Statistics__StatListItemLabel}>
